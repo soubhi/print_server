a870c9117f750fe0e2cab9bf608beb16ed0dbadc
shashank3011 <anihtars@gmail.com>
2018-12-06 13:00:20 +0530

    New Config file

--- ps/login.php
+++ ps/login.php
@@ -10,8 +10,6 @@
 	// SETTING THINGS
 	$error = '';
 	include("config.php");
-	/*************************  VARIABLE ****************************/
-	$password_path = "/home/printmaster/ypcat_passwd_20181201.txt";
 
 	// AFTER GETTING USERNAME AND PASSWORD
 	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
